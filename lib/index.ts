c4df50fe45ea71b86dd2c1dc9b62b9f25d7bb50b
UsboKirishima <usbertibox@gmail.com>
2022-04-09 21:58:57 +0200

    feat!: code doesn't work

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,50 @@
-import colors from "colors"
+import colors from "colors";
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import { ascii, ascii_info } from "../config/ascii"
 import { isNumberObject } from "node:util/types";
+import axios from 'axios';
 
 colors.enable();
+
+let webhooks: number,
+    urls: string[],
+    avatarURL: string,
+    username: string,
+    content: string;
 
 const cherry = async (): Promise<void | any> => {
     console.clear();
 
     await print_ascii();
 
-    let webhooks = await req_threads();
+    webhooks = await req_threads();
 
     console.clear();
 
     await print_ascii();
 
-    let urls = await wh_url(webhooks);
+    urls = await wh_url(webhooks);
 
-    //console.log(urls);
+    console.clear();
+
+    await print_ascii();
+
+    avatarURL = await getAvatar();
+
+    console.clear();
+
+    await print_ascii();
+
+    username = await getUsername();
+
+    console.clear();
+
+    await print_ascii();
+
+    content = await getContent();
+
+    await spamMessages();
 }
 
 const print_ascii = async (): Promise<void> => {
@@ -29,7 +54,7 @@ const print_ascii = async (): Promise<void> => {
 
 const req_threads = async (): Promise<number> => {
     let rl = readline.createInterface({ input, output });
-    let answer: string | number = await rl.question('[+] Insert the number of webhook: '.blue);
+    let answer: string | number = await rl.question('[?] Insert the number of webhook: '.blue);
     if (typeof answer !== 'string') {
         cherry();
     }
@@ -76,7 +101,7 @@ const req_threads = async (): Promise<number> => {
     return answer;
 }
 
-const wh_url = async (wh: number) => {
+const wh_url = async (wh: number): Promise<string[]> => {
     let url: string[] = [];
     for (let i = 1; i < wh + 1; i++) {
         let rl = readline.createInterface({ input, output });
@@ -88,6 +113,83 @@ const wh_url = async (wh: number) => {
         url.push(answer);
     }
     return url;
+}
+
+const getAvatar = async (): Promise<string> => {
+    let rl = readline.createInterface({ input, output });
+    let answer: string = await rl.question(`[?] Insert the webhook avatar url (default): `.blue);
+    if (typeof answer !== 'string') {
+        cherry();
+    }
+    if (answer.toLowerCase() == 'default')
+        return 'https://pbs.twimg.com/profile_images/1303109604688224257/3zsnQHJq_400x400.jpg';
+    rl.close()
+    return answer;
+}
+
+const getUsername = async (): Promise<string> => {
+    let rl = readline.createInterface({ input, output });
+    let answer: string = await rl.question(`[?] Insert the webhook username: `.blue);
+    if (typeof answer !== 'string') {
+        cherry();
+    }
+    if (answer == '') await cherry();
+    rl.close()
+    return answer;
+}
+
+const randomText = async (length: number): Promise<string> => {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * length));
+    }
+    return result;
+}
+
+const getContent = async (): Promise<string> => {
+    let rl = readline.createInterface({ input, output });
+    let answer: string = await rl.question(`[?] Insert the webhook message content\n[trandom] [urandom] [everyone] [here]: `.blue);
+    if (typeof answer !== 'string') {
+        cherry();
+    }
+    if (!answer) await cherry();
+
+    answer.replace('[trandom]', await randomText(10)),
+        answer.replace('[urandom]', 'https://picsum.photos/200/300?random=1'),
+        answer.replace('[everyone]', '@everyone'),
+        answer.replace('[here]', '@here');
+
+    rl.close()
+    return answer;
+}
+
+const sendMessages = async (): Promise<void> => {
+    urls.forEach(async (w): Promise<void | any> => {
+        await axios.post(w, {
+            username: username,
+            content: content,
+            avatar_url: avatarURL,
+        })
+            .then(async (response) => {
+                console.log("[+] webhook sent!".green)
+            })
+            .catch(async (error) => {
+                console.log("[-] an error occurred.".red);
+            });
+    })
+}
+
+const spamMessages = async (): Promise<void> => {
+    try {
+        while(true) {
+            await sendMessages().then(async () => {
+                console.log('[+] message successfully sent!'.green);
+            })
+        }
+    } catch(error) {
+        await console.error(error);
+    }
 }
 
 (async (): Promise<void | any> => {
